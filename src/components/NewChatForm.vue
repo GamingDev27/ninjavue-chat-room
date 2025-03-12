0a0d2f8a39832276f74c2c435ef432f640e859f2
Corvinus <markus.corvinus0827@gmail.com>
2025-03-12 22:40:14 +0800

    minor fix

--- src/components/NewChatForm.vue
+++ src/components/NewChatForm.vue
@@ -1,8 +1,15 @@
 <template>
-    <form>
-        <textarea name="chat" id="chat" cols="30" rows="2" placeholder="Type a message and hit enter..." v-model="message" @keypress.enter.prevent="handleChat"></textarea>
-    
-    </form>
+  <form>
+    <textarea
+      name="chat"
+      id="chat"
+      cols="30"
+      rows="2"
+      placeholder="Type a message and hit enter..."
+      v-model="message"
+      @keypress.enter.prevent="handleChat"
+    ></textarea>
+  </form>
 </template>
 
 <script setup>
@@ -16,33 +23,33 @@ const { user } = getUser()
 const { addDoc, error } = useCollection('chats')
 
 const handleChat = async () => {
-    const chat = {
-        message: message.value,
-        user: user.value,
-        createdAt: timestamp()
-    }
-    
-    await addDoc(chat)
-    
-    if (!error.value) {
-        message.value = ''
-    }
+  const chat = {
+    message: message.value,
+    sender: user.value.displayName,
+    createdAt: timestamp(),
+  }
+
+  await addDoc(chat)
+
+  if (!error.value) {
+    message.value = ''
+  }
 }
 </script>
 
 <style>
-    form {
-        margin: 10px;
-    }
-    textarea {
-        width: 100%;
-        max-width: 100%;
-        margin-bottom: 6px;
-        padding: 10px;
-        box-sizing: border-box;
-        border: 0;
-        border-radius: 20px;
-        font-family: inherit;
-        outline: none;
-    }
+form {
+  margin: 10px;
+}
+textarea {
+  width: 100%;
+  max-width: 100%;
+  margin-bottom: 6px;
+  padding: 10px;
+  box-sizing: border-box;
+  border: 0;
+  border-radius: 20px;
+  font-family: inherit;
+  outline: none;
+}
 </style>
